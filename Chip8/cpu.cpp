da902484e830a0c20b2f0c02d873eda4113dbc3f
eldardamirov <eldardamirov75@mail.ru>
2019-06-27 17:27:44 +0300

    CPU memory formed.

--- Chip8/cpu.cpp
+++ Chip8/cpu.cpp
@@ -6,12 +6,27 @@
 //  Copyright © 2019 Эльдар Дамиров. All rights reserved.
 //
 
+#include <array>
+
+using tiny = char;
+
 class Cpu
     {
     public:
-    
+        Cpu(): registerI ( 200 ), stackCounter ( 0 ), memoryOffsetCounter ( 200 ), delayTimer ( 0 ), soundTimer ( 0 ){}
         
     private:
+        std::array <tiny, 4096> ram{};
+        std::array <int, 16> stack{}; 
+        std::array <tiny, 16> registers{};
+        
+        int registerI{};
+        tiny stackCounter{};
+        tiny memoryOffsetCounter{};
+        
+        int delayTimer{};
+        int soundTimer{};
+        
         
     
     };
