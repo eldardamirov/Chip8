f9edabf8820c741d52b1d4dfceaea41125218602
eldardamirov <eldardamirov75@mail.ru>
2019-06-27 16:02:13 +0300

    Initial Commit

---
+++ Chip8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chip8
+//
+//  Created by Эльдар Дамиров on 27.06.2019.
+//  Copyright © 2019 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
