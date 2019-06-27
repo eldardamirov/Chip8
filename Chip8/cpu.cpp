a975ede04b62c7b2126126771cc6f421c543852c
eldardamirov <eldardamirov75@mail.ru>
2019-06-27 16:12:23 +0300

    cpu.cpp file added

--- Chip8/cpu.cpp
+++ Chip8/cpu.cpp
@@ -1,16 +1,9 @@
 //
-//  main.cpp
+//  cpu.cpp
 //  Chip8
 //
 //  Created by Эльдар Дамиров on 27.06.2019.
 //  Copyright © 2019 Эльдар Дамиров. All rights reserved.
 //
 
-#include <iostream>
 
-int main() 
-    {
-    
-    
-    return 0;
-    }
